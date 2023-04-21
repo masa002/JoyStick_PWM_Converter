3d53470bbbfdc09cb9c4c7dab67cae72aa5078fd
masa002 <m.obara.sys21@morijyobi.ac.jp>
2023-04-21 12:45:15 +0900

    Fix code

--- sub.py
+++ sub.py
@@ -172,7 +172,6 @@ class GraphFrame(wx.Frame):
         self.axes = self.figure.add_subplot(111)
         self.canvas = FigureCanvas(self, -1, self.figure)
         self.controller_input = ControllerInput()
-        self.deadzone = 0.1
         self.update_controller_input()
 
         self.points = [(0, 0), (1, 1)]
@@ -244,7 +243,7 @@ class GraphFrame(wx.Frame):
         self.canvas.draw()
 
     def update_controller_input(self):
-        self.controller_input.update(self.deadzone)
+        self.controller_input.update()
         wx.CallLater(10, self.update_controller_input)
 
     def load_graph_points(self):
@@ -267,7 +266,6 @@ class GraphFrame(wx.Frame):
 class ControllerInput:
     def __init__(self):
         self.pwm_control_active = False
-        self.deadzone = 0.1
 
         pygame.init()
         pygame.joystick.init()
@@ -279,7 +277,8 @@ class ControllerInput:
 
     def start_pwm_control(self):
         self.pwm_control_active = True
-        self.load_key_bindings()
+        # self.load_key_bindings()
+        self.get_deadzone()
 
     def stop_pwm_control(self):
         self.pwm_control_active = False
@@ -287,11 +286,20 @@ class ControllerInput:
     def get_axis(self, axis):
         return self.joystick.get_axis(axis)
 
+    def get_deadzone(self):
+        # graph_points.jsonの1つ目の点のy座標をデッドゾーンとする
+        try:
+            with open("config/graph_points.json", "r") as f:
+                self.x_deadzone, self.y_deadzone, self.z_deadzone = [point[0][1] for point in json.load(f).values()]
+                print(self.x_deadzone, self.y_deadzone, self.z_deadzone)
+        except:
+            self.x_deadzone, self.y_deadzone, self.z_deadzone = 0.1, 0.1, 0.1
+
     def pwm_control(self, value, threshold):
         if abs(value) < threshold:
             return 0
         else:
-            return np.interp(value, [-1, 1], [-100, 100])  # PWM周期の軸を-100〜100に変更
+            return 1 / (1 - threshold) * (abs(value) - threshold) * value / abs(value)
         
     def key_press(self, key):
         win32api.keybd_event(ord(key), 0, 0, 0)
@@ -314,9 +322,7 @@ class ControllerInput:
         except:
             self.key_bindings = {}
 
-    def update(self, deadzone=None):
-        if deadzone is None:
-            deadzone = self.deadzone
+    def update(self):
         for event in pygame.event.get():
             pass
 
@@ -325,15 +331,26 @@ class ControllerInput:
             y_axis = self.get_axis(1)
             z_axis = self.get_axis(2)
 
-            x_pwm = self.pwm_control(x_axis, self.deadzone)
-            y_pwm = self.pwm_control(y_axis, self.deadzone)
-            z_pwm = self.pwm_control(z_axis, self.deadzone)
+            x_pwm = self.pwm_control(x_axis, self.x_deadzone)
+            y_pwm = self.pwm_control(y_axis, self.y_deadzone)
+            z_pwm = self.pwm_control(z_axis, self.z_deadzone)
 
             self.pwm_button_click(x_pwm, "X")
             self.pwm_button_click(y_pwm, "Y")
             self.pwm_button_click(z_pwm, "Z")
 
             print(f"X_PWM: {x_pwm}, Y_PWM: {y_pwm}, Z_PWM: {z_pwm}")
+
+class KeyboardOutput:
+    def __init__(self):
+        self.load_key_bindings()
+
+    def load_key_bindings(self):
+        try:
+            with open("config/key_bindings.json", "r") as f:
+                self.key_bindings = json.load(f)
+        except:
+            self.key_bindings = {"X": {"negative": "", "positive": ""}, "Y": {"negative": "", "positive": ""}, "Z": {"negative": "", "positive": ""}}
 
 def main():
     app = wx.App()
